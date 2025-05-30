041334660e142a1574e214937827de2d2bf5ab7b
Vinicius Fernandes <fernandes.vinicius@outlook.com.br>
2025-05-29 22:07:54 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá, Vite com React e TypeScript!</h1>
+      <p>Este é um exemplo de aplicação usando Vite, React e TypeScript.</p>
+    </div>
+  );
+}
+
+export default App;
