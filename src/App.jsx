ea24761dcf47f7b57d596d156bd0071e980c81d0
Vinicius Fernandes <fernandes.vinicius@outlook.com.br>
2025-05-29 22:00:51 -0300

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Olá, Vite com React!</h1>
+      <p>Este é um exemplo de aplicação usando Vite e React.</p>
+    </div>
+  );
+}
+
+export default App;
